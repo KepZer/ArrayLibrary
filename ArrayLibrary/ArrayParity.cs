603b6b6342ea319d57773bcf03d62efcc40775f1
KepZer <KepLordZer@gmail.com>
2021-11-26 07:47:04 +0300

    Добавьте файлы проекта.

---
+++ ArrayLibrary/ArrayParity.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class ArrayParity
+{
+    public int CalcParity(Array array)
+    {
+        int res = 0;
+        foreach(int num in array)
+        {
+            if (num % 2 == 0) res++;
+        }
+        return res;
+    }
+}
